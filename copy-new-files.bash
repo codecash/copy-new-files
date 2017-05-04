e3957ed2ce2615af3809fbb7219848cf6f491810
Jeff Puckett <jeff@jeffpuckett.com>
2017-05-04 02:56:33 +0000

    add copy new script

---
+++ copy-new-files.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+COPYNEW_SOURCE_DIRECTORY=""
+COPYNEW_DESTIN_DIRECTORY=""
+
+cd $(dirname "${BASH_SOURCE[0]}")
+if [ -f .env ]; then
+  source .env
+fi
+
+# validate folders exist
+if [[ ! -d "$COPYNEW_SOURCE_DIRECTORY" ]] || [[ ! -d "$COPYNEW_DESTIN_DIRECTORY" ]]; then
+  echo "Error: invalid COPYNEW_SOURCE_DIRECTORY or COPYNEW_DESTIN_DIRECTORY Check source code. Aborting."
+  exit 1
+fi
+
+touch .copynew .copyold
+
+ls -1 "$COPYNEW_SOURCE_DIRECTORY" > .copynew
+
+for file in $(cat .copynew); do
+  found=0
+  for old in $(cat .copyold); do
+    if [ "$file" == "$old" ]; then
+      found=1
+      break
+    fi
+  done
+  if [ $found = 0 ]; then
+    cp -r "$COPYNEW_SOURCE_DIRECTORY/$file" "$COPYNEW_DESTIN_DIRECTORY/$file"
+  fi
+done
+
+# make sure tempfile not empty so as to not overwrite
+# extract log in event of connection issues, errors, etc.
+if [ -s .copynew ]; then
+  mv .copynew .copyold
+fi
